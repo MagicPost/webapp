61ca088f0e0a2729f1d05240b20d95fcec11ac1b
Le Viet Dat <lvdat.roy@gmail.com>
2023-11-24 20:55:43 +0700

    chore: commit missing files

--- src/components/Logo.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from '@/lib/utils';
-
-export default function Logo({ className }: { className?: string }) {
-  return <h1 className={cn('text-2xl font-bold', className)}>MagicPost</h1>;
-}
